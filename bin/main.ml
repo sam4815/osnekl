9d641c48e4d92f1827757f4ea2bda4eeed70acac
sam <sam.brougham@gmail.com>
2022-12-29 22:32:35 -0500

    Initialize dune project

---
+++ bin/main.ml
@@ -0,0 +1,13 @@
+open! Notty
+open! Notty_unix
+
+
+let rec main_loop t =
+  let img = I.(string A.(bg lightred ++ fg black) "This is a simple example") in
+    Term.image t img;
+    match Term.event t with
+    | `End | `Key (`Escape, []) -> ()
+    | _ -> main_loop t
+
+let () =
+  let t = Term.create () in main_loop t
