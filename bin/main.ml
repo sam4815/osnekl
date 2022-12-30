d526be51bbe30b355b8e5de69427fc33b4fc5452
sam <sam.brougham@gmail.com>
2022-12-30 08:28:22 -0500

    Add formatting

--- bin/main.ml
+++ bin/main.ml
@@ -1,13 +1,11 @@
 open! Notty
 open! Notty_unix
 
-
 let rec main_loop t =
-  let img = I.(string A.(bg lightred ++ fg black) "This is a simple example") in
-    Term.image t img;
-    match Term.event t with
-    | `End | `Key (`Escape, []) -> ()
-    | _ -> main_loop t
+  let img = I.(string A.(bg lightred ++ fg black) "This is a great example") in
+  Term.image t img;
+  match Term.event t with `End | `Key (`Escape, []) -> () | _ -> main_loop t
 
 let () =
-  let t = Term.create () in main_loop t
+  let t = Term.create () in
+  main_loop t
--- lib/osnekl.ml
+++ lib/osnekl.ml
@@ -1,7 +1,4 @@
 open! Notty
 open! Notty_unix
 
-let () =
-I.string A.(fg lightred) "Wow!"
-|> eol
-|> Notty_unix.output_image
+let () = I.string A.(fg lightred) "Wow!" |> eol |> Notty_unix.output_image
