9431891aab6167b3a5ef46362b3fa406284f8315
sam <sam.brougham@gmail.com>
2023-01-02 13:55:10 -0500

    Render game to terminal

--- bin/main.ml
+++ bin/main.ml
@@ -3,10 +3,14 @@ open Notty
 open Osnekl
 module Term = Notty_lwt.Term
 
-let counter = ref 0
-
 let game =
-  Game.create ~height:50 ~width:50 ~initial_snake_length:3 ~amount_to_grow:3
+  ref
+    (Game.create ~height:10 ~width:10 ~initial_snake_length:3 ~amount_to_grow:3)
+
+let rec step_game () =
+  Lwt_unix.sleep 0.1 >>= fun () ->
+  Game.step !game;
+  Lwt.return () >>= fun () -> step_game ()
 
 let grid xxs = xxs |> List.map I.hcat |> I.vcat
 
@@ -23,35 +27,28 @@ let outline attr t =
   in
   grid [ [ a; hbar; b ]; [ vbar; I.void (w - 2) 1; vbar ]; [ d; hbar; c ] ]
 
-let rec increase_counter () =
-  Lwt_unix.sleep 0.1 >>= fun () ->
-  if !counter < max_int then counter := !counter + 1 else counter := 0;
-  Lwt.return () >>= fun () -> increase_counter ()
+let draw_block (_, h) { Position.row; col } (color : A.color) =
+  let dot : image = I.uchar A.(fg color) (Uchar.of_int 0x25cf) 1 1 in
+  I.pad ~t:(h - row - 2) ~l:(col + 1) dot
 
-let draw_block { Position.row; col } =
-  let dot : image = I.uchar A.(fg lightgreen) (Uchar.of_int 0x25cf) 1 1 in
-  I.pad ~t:(row + 2) ~l:(col + 2) dot
-
-let draw_snake =
-  let snake = Game.snake game in
-  draw_block (Snake.head_location snake)
-(* Snake head is a different color *)
-(* draw_block ~color:Colors.head_color (List.hd_exn snake_locations) *)
-
-let draw_game _ =
-  (* let draw_block col row = *)
-  let dot : image = I.uchar A.(fg lightred) (Uchar.of_int 0x25cf) 10 10 in
-  let double_dot : image =
-    I.uchar A.(fg lightgreen) (Uchar.of_int 0x25cf) 20 20
+let draw_snake (w, h) snake =
+  let snake_locations : Position.t list = Snake.locations snake in
+  let locations =
+    List.map (fun pos -> draw_block (w, h) pos A.lightgreen) snake_locations
   in
-  let wut =
-    if !counter < 5 then I.pad ~t:10 ~l:10 double_dot else I.pad ~t:10 ~l:10 dot
-  in
-  I.(draw_snake </> wut)
+  List.fold_left
+    (fun a b -> I.(a </> b))
+    (draw_block (w, h) (Snake.head_location snake) A.green)
+    locations
 
-let render term (w, h) =
-  I.(outline A.(fg lightred) term </> draw_game { Position.col = w; row = h })
+let draw_apple (w, h) apple = draw_block (w, h) (Apple.location apple) A.red
 
+let draw_game (w, h) =
+  let apple = draw_apple (w, h) (Game.apple !game) in
+  let snake = draw_snake (w, h) (Game.snake !game) in
+  I.(snake </> apple)
+
+let render term (w, h) = I.(outline A.(fg lightred) term </> draw_game (w, h))
 let timer () = Lwt_unix.sleep 0.1 >|= fun () -> `Timer
 
 let event term =
@@ -63,7 +60,16 @@ let rec loop term (e, t) dim =
   e <?> t >>= function
   | `End | `Key (`Escape, []) -> Lwt.return_unit
   | `Key (`Arrow `Up, []) ->
-      counter := 0;
+      Game.set_direction !game Up;
+      loop term (event term, t) dim
+  | `Key (`Arrow `Down, []) ->
+      Game.set_direction !game Down;
+      loop term (event term, t) dim
+  | `Key (`Arrow `Left, []) ->
+      Game.set_direction !game Left;
+      loop term (event term, t) dim
+  | `Key (`Arrow `Right, []) ->
+      Game.set_direction !game Right;
       loop term (event term, t) dim
   | `Timer ->
       Term.image term (render term dim) >>= fun () ->
@@ -78,8 +84,10 @@ let interface () =
   let tc = Unix.(tcgetattr stdin) in
   Unix.(tcsetattr stdin TCSANOW { tc with c_isig = false });
   let term = Term.create () in
-  let size = Term.size term in
-  loop term (event term, timer ()) size
+  let w, h = Term.size term in
+  game :=
+    Game.create ~height:h ~width:w ~initial_snake_length:3 ~amount_to_grow:3;
+  loop term (event term, timer ()) (w, h)
 
-let main () = Lwt.choose [ increase_counter (); interface () ]
+let main () = Lwt.choose [ step_game (); interface () ]
 let () = Lwt_main.run (main ())
